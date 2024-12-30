a80fc0384cf8b2fd53974efc08a5303433d24e48
LordTebogo <manyubeletebogo@gmail.com>
2024-12-30 16:13:10 +0200

    Initial commit

---
+++ bizapp/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from products.views import ProductViewSet
+from django.contrib import admin
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+router = DefaultRouter()
+
+router.register("products", ProductViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("api/", include(router.urls)),
+]
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
